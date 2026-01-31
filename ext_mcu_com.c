e6cb7e9826ab20e172c4e2437a79d2c0a78b80b8
Chimipupu <chiminyanyan@gmail.com>
2026-01-31 15:32:24 +0900

    SPIのマスターかスレーブかはコンパイルスイッチで切り替え

--- ext_mcu_com.c
+++ ext_mcu_com.c
@@ -5,7 +5,7 @@
  * @version 0.1
  * @date 2026-01-31
  * 
- * @copyright Copyright (c) 2025 Chimipupu All Rights Reserved.
+ * @copyright Copyright (c) 2026 Chimipupu All Rights Reserved.
  * 
  */
 
@@ -13,6 +13,7 @@
 
 // --------------------------------------------------------------------------
 // [Private]
+#ifdef EMC_MODE_SLAVE
 typedef void(*p_func_cmd_cbk)(uint8_t *p_data, uint32_t data_len);
 void emc_cmd_config_cbk(uint8_t *p_data, uint32_t data_len);
 typedef struct {
@@ -23,41 +24,46 @@ typedef struct {
 const emc_cmd_tbl_data_t g_emc_cmd_tbl[] = {
     {0x10, emc_cmd_config_cbk}
 };
+#endif
 
 static emc_config_t s_emc_confg;
 static bool s_is_init_fail = false;
 // --------------------------------------------------------------------------
 // [Static関数]
 
+#ifdef EMC_MODE_SLAVE
 void emc_cmd_config_cbk(uint8_t *p_data, uint32_t data_len)
 {
     // TOOD
 }
-
+#endif
 // --------------------------------------------------------------------------
 // [API関数]
 
 bool emc_init(emc_config_t *p_config)
 {
-    if ((p_config->mode > EMC_MODE_SLAVE) ||
-        (p_config->p_func_read == NULL)   ||
+    if ((p_config->p_func_read == NULL)   ||
+#ifdef EMC_MODE_MASTER
         (p_config->p_func_write == NULL)  ||
+#endif
         (p_config->p_func_delay == NULL)
     ) {
         s_is_init_fail = true;
         return false;
     }
 
-    if ((p_config->mode == EMC_MODE_MASTER) && (p_config->p_func_cs != NULL)){
+#ifdef EMC_MODE_MASTER
+    if (p_config->p_func_cs != NULL){
         s_emc_confg.p_func_cs    = p_config->p_func_cs;
     } else {
         s_is_init_fail = true;
         return false;
     }
 
-    s_emc_confg.mode         = p_config->mode;
-    s_emc_confg.p_func_read  = p_config->p_func_read;
     s_emc_confg.p_func_write = p_config->p_func_write;
+#endif
+
+    s_emc_confg.p_func_read  = p_config->p_func_read;
     s_emc_confg.p_func_delay = p_config->p_func_delay;
 
     return true;
@@ -72,23 +78,15 @@ void emc_cmd_proc(emc_cmd_frame_t *p_flame)
         return;
     }
 
-    switch (s_emc_confg.mode)
-    {
-        case EMC_MODE_MASTER:
-            s_emc_confg.p_func_cs(SPI_CS_LOW); // SPI CS = Low
-            s_emc_confg.p_func_write(p_flame->cmd);
-            s_emc_confg.p_func_write(p_flame->tx_data);
-            p_flame->rx_data = s_emc_confg.p_func_read();
-            s_emc_confg.p_func_cs(SPI_CS_HIGH); // SPI CS = High
-            break;
-
-        case EMC_MODE_SLAVE:
-            p_flame->cmd = s_emc_confg.p_func_read();
-            p_flame->rx_data = s_emc_confg.p_func_read();
-            break;
-
-        default:
-            // MOP
-            break;
-    }
+#ifdef EMC_MODE_MASTER
+    s_emc_confg.p_func_cs(SPI_CS_LOW); // SPI CS = Low
+    s_emc_confg.p_func_write(p_flame->cmd);
+    s_emc_confg.p_func_write(p_flame->tx_data);
+    p_flame->rx_data = s_emc_confg.p_func_read();
+    s_emc_confg.p_func_cs(SPI_CS_HIGH); // SPI CS = High
+#else
+    p_flame->cmd = s_emc_confg.p_func_read();
+    p_flame->rx_data = s_emc_confg.p_func_read();
+    // TOOD: コマンド解析処理の実装
+#endif
 }
--- ext_mcu_com.h
+++ ext_mcu_com.h
@@ -15,9 +15,12 @@
 #include <stdbool.h>
 #include <string.h>
 
+// コンパイルスイッチ
 #define EMC_MODE_MASTER     0x00
-#define EMC_MODE_SLAVE      0x01
-#define EMC_MODE_NONE       0xFF
+// #define EMC_MODE_SLAVE      0x01
+#if defined(EMC_MODE_MASTER) && defined(EMC_MODE_SLAVE)
+#error "Please select either SPI master or SPI slave as the compile switch"
+#endif
 
 #define SPI_CS_LOW          0x00
 #define SPI_CS_HIGH         0x01
@@ -27,8 +30,8 @@ typedef void(*p_func_drv_spi_write)(uint8_t tx_data);
 typedef uint8_t(*p_func_drv_spi_read)(void);
 typedef void(*p_func_delay_ms)(uint32_t ms);
 
+#ifdef EMC_MODE_MASTER
 typedef struct {
-    uint8_t mode;
     p_func_drv_spi_cs p_func_cs;       // 呼び元のSPIドライバ CS関数ポインタ
     p_func_drv_spi_write p_func_write; // 呼び元のSPIドライバ Write関数ポインタ
     p_func_drv_spi_read p_func_read;   // 呼び元のSPIドライバ Read関数ポインタ
@@ -40,6 +43,17 @@ typedef struct {
     uint8_t tx_data;
     uint8_t rx_data;
 } emc_cmd_frame_t;
+#else
+typedef struct {
+    p_func_drv_spi_read p_func_read;   // 呼び元のSPIドライバ Read関数ポインタ
+    p_func_delay_ms p_func_delay;      // 呼び元のmsec遅延するdelay関数ポインタ
+} emc_config_t;
+
+typedef struct {
+    uint8_t cmd;
+    uint8_t rx_data;
+} emc_cmd_frame_t;
+#endif
 
 bool emc_init(emc_config_t *p_config);
 void emc_cmd_proc(emc_cmd_frame_t *p_flame);
